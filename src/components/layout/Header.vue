73cbf747a113ebe2219ec0f243b8d0797c00f26f
Satch09 <18578121+Satch09@users.noreply.github.com>
2020-02-25 18:40:53 +0200

    commit before placeholder api

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -2,13 +2,19 @@
   <header class="header">
     <h1>{{phrase}}</h1>
     <h2>{{name}}</h2>
+    <h2>{{text}}</h2>
   </header>
 </template>
 
 <script>
 export default {
   name: "Header",
-  props: ["phrase", "name"]
+  props: ["phrase", "name"], // These refer to external properties which the component is able to accept
+  data() {
+    return {
+      text: "This is the default banner"
+    };
+  }
 };
 </script>
 
